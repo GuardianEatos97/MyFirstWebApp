139f24b75c956366fdc5798867bbd32b7982d113
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-07-01 13:56:15 +0200

    Changes

---
+++ MyFirstWebApp/Controllers/AthletesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApp.Database;
+using MyFirstWebApp.Models;
+using MyFirstWebApp.Models.Entities;
+
+namespace MyFirstWebApp.Controllers
+{
+    public class AthletesController : Controller
+    {
+        private readonly AppDatabaseContext databaseContext;
+        public AthletesController(AppDatabaseContext databaseContext)
+        {
+            this.databaseContext = databaseContext; 
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddAthleteVM athleteVM) 
+        {
+            var athlete = new Athlete
+            {
+                Name = athleteVM.Name,
+                Email = athleteVM.Email,
+                PhoneNumber = athleteVM.PhoneNumber,
+                Injured = athleteVM.Injured,
+            };
+            await databaseContext.Athletes.AddAsync(athlete);
+            await databaseContext.SaveChangesAsync();
+            return View();
+        }
+    }
+}
---
+++ MyFirstWebApp/Models/AddAthleteVM.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstWebApp.Models
+{
+    public class AddAthleteVM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool Injured { get; set; }
+    }
+}
